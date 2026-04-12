302a19809d6e76dccd8f71de3e3036b41b232e12
Aleksey <altrofimov1990@gmail.com>
2026-04-12 12:56:02 +0300

    Задание 3: исправление

--- lesson_01/lesson_1_task_3.py
+++ lesson_01/lesson_1_task_3.py
@@ -1,3 +1,3 @@
-first_name = input("Алексей")
-last_name = input("Трофимов")
-print("Вас зовут: +last_name, +first_name")
+first_name = input("Введите ваше имя: ")
+last_name = input("Введите вашу фамилию: ")
+print("Вас зовут: " + last_name + " " + first_name)
